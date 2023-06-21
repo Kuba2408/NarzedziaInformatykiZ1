c96cb7f1f656b5a142c72ace04a0424921410f8a
Kuba2408 <jakub.wieczorek.2408@gmail.com>
2023-06-21 23:38:41 +0200

    Revert "ask for name/surname"
    This reverts commit 85a9890bf59cbc327d6f623512d5791cf61cc16c.

--- file2.java
+++ file2.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("file 1");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Jak masz na nazwisko?");
-        String surname = sc.nextLine();
-        System.out.println("Masz na nazwisko " + surname);
+        System.out.println("file 2");
     }
 }
